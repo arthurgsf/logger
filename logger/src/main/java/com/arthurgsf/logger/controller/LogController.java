adebc3e33e5284a8d3eaafa8685f8c272ea48e9e
Arthur <arthurgsf@nca.ufma.br>
2020-12-16 19:30:19 -0300

    Log Controller e Teste de Log Controller Adicionados

---
+++ logger/src/main/java/com/arthurgsf/logger/controller/LogController.java
@@ -0,0 +1,34 @@
+package com.arthurgsf.logger.controller;
+
+import java.util.List;
+
+import com.arthurgsf.logger.model.entity.Log;
+import com.arthurgsf.logger.model.entity.Maquina;
+import com.arthurgsf.logger.service.LogService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/log")
+public class LogController {
+    @Autowired
+    LogService logSrv;
+
+    @GetMapping("/{id}")
+    public ResponseEntity buscar(@PathVariable("id") Long idMaquina){
+        try{
+            Log l = Log.builder().maquina(Maquina.builder().id(idMaquina).build()).build();
+            List<Log> lista = logSrv.buscar(l);
+            return new ResponseEntity(lista, HttpStatus.OK);
+
+        }catch(RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
