e5ddc03dd4fae7b9f51e971ed12a6e46f2cd920f
Arthur <arthurgsf@nca.ufma.br>
2020-12-16 16:37:29 -0300

    DTO's adicionados

---
+++ logger/src/main/java/com/arthurgsf/logger/model/dto/LogDTO.java
@@ -0,0 +1,24 @@
+package com.arthurgsf.logger.model.dto;
+
+import java.time.LocalDateTime;
+
+import com.arthurgsf.logger.model.entity.Maquina;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogDTO {
+    private long id;
+    private double consumoCpu;
+    private double consumoMemoria;
+    private double consumoDisco;
+    private double consumoGpu;
+    private LocalDateTime data;
+    private Maquina usuario;
+}
