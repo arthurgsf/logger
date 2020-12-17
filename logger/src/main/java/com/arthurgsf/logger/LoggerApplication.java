c4efa459430cf5b8d80ed03026e323439f2ec118
Arthur <arthurgsf@nca.ufma.br>
2020-12-16 22:39:24 -0300

    removendo bloqueio de CORS

--- logger/src/main/java/com/arthurgsf/logger/LoggerApplication.java
+++ logger/src/main/java/com/arthurgsf/logger/LoggerApplication.java
@@ -2,10 +2,18 @@ package com.arthurgsf.logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class LoggerApplication {
-
+@EnableWebMvc
+public class LoggerApplication implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry cors) {
+		cors.addMapping("/**")
+			.allowedMethods("GET", "PUT", "DELETE", "POST", "OPTIONS");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(LoggerApplication.class, args);
 	}
